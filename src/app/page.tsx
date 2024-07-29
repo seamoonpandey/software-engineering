10bbdc5c1ec5b9359fc1071e3055d61b56ed6f1e
Seamoon Pandey <hackedasocial@gmail.com>
2024-07-30 01:23:08 +0545

    add cta and smoking hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Hero from "@/components/Hero";
 import Values from "@/components/Values";
 import Categories from "@/components/Categories";
 import HowItWorks from "@/components/HowItWorks";
+import CTA from "@/components/CTA";
+import SmokinHero from "@/components/SmokinHero";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <Values />
       <Categories />
       <HowItWorks />
+      <CTA />
+     <SmokinHero /> 
     </>
   );
 }
