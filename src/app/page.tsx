aa42eee7a4bf331d073447e428576fecfc742f96
Seamoon Pandey <hackedasocial@gmail.com>
2024-07-30 01:09:47 +0545

    add how it works section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import Values from "@/components/Values";
 import Categories from "@/components/Categories";
+import HowItWorks from "@/components/HowItWorks";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Values />
       <Categories />
+      <HowItWorks />
     </>
   );
 }
